0c01a677f0f3979b1abbcf4b7aa4f7ab8e6858a5
bjlagman12 <bjlagman@gmail.com>
2019-05-16 12:12:09 -0700

    Refactor Api calls

--- server/server.js
+++ server/server.js
@@ -4,4 +4,9 @@ const port = 3001
 
 app.use(express.static('./public/dist'))
 
+app.use('/review/:id' , (req, res) => {
+  
+})
+
+
 app.listen(port, () => console.log(`LISTENING TO ANDRE PORT ${port}`));
