4a31ab0e2108cc18e956a1b18d05e2faaf206613
bjlagman12 <bjlagman@gmail.com>
2019-05-16 14:32:56 -0700

    Set up api calls

--- server/models/modal.js
+++ server/models/modal.js
@@ -1,1 +1,17 @@
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
+// const  { Review } = require('../orm_schema');
+
+// const getHouse = (id) => {
+//   return Review.find({ house_id: id })
+//     .then((data) => {
+//       console.log(data)
+//       return data
+//     })
+//     .catch((err) => {
+//       console.log(err, 'error');
+//     });
+// }
+
+// module.exports = {
+//   getHouse
+// }
--- server/server.js
+++ server/server.js
@@ -2,11 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3001
 
+const bodyParser = require('body-parser')
+const { getHouse } = require('./models/modal');
+
+app.use(bodyParser.json())
+
 app.use(express.static('./public/dist'))
 
 app.use('/review/:id' , (req, res) => {
-  
-})
-
+  const id = req.params.id
+  // getHouse(id, (err, data) => {
+  //   if (err) {
+  //     res.status(404).end();
+  //   } else {
+  //     res.status(202).send(data)
+  //   }
+  // })
+});
 
 app.listen(port, () => console.log(`LISTENING TO ANDRE PORT ${port}`));
