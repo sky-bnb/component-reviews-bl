2b47256c64cf87576a4a60f25c950cc913234fb1
bjlagman12 <bjlagman@gmail.com>
2019-05-20 14:22:51 -0700

    Create the review for each listing

--- src/componenets/ReviewList.jsx
+++ src/componenets/ReviewList.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReviewListEntry from './ReviewListEntry';
 
 const ReviewList = () => (
-  <div className="allReview">
+  <div className="allReviews">
+    <ReviewListEntry />
+    <ReviewListEntry />
     <ReviewListEntry />
     <ReviewListEntry />
     <ReviewListEntry />
--- src/componenets/ReviewListEntry.jsx
+++ src/componenets/ReviewListEntry.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../ReviewListEntry.css';
 
 const ReviewListEntry = () => (
   <div className="eachReview">
     <div className="top">
       <div className="userAv">
-        image goes here
+        <img src="https://s3.amazonaws.com/uifaces/faces/twitter/chris_frees/128.jpg" alt="" />
       </div>
       <div className="userInfo">
         <div className="userName">
@@ -14,16 +15,14 @@ const ReviewListEntry = () => (
           April 2019
         </div>
       </div>
-      <div className="mid">
-        <div>
-          this is where the description goes
-        </div>
+    </div>
+    <div className="mid">
+      <div className="description">
+      Unfortunately, I cannot recommend this home, the cigarette smell was horrific in the bedroom. Add to that the cheapest linens, towels and uncomfortable beds I have ever seen in a “luxury home”. We literarily had to leave after one night and check into a hotel. For anyone looking for a luxury experience, don’t be fooled by the nice pictures as that does not tell the full story about this home. Living the dream was easy to communicate but unfortunately, don’t “get it” when it comes to what a luxury home renter is really looking for. If you're looking to host a party or show off nice Instagram pictures, this house might work perfectly for you, just don’t expect to sleep here.
       </div>
-      <div className="bottom">
-        <div>
-          ----------------------line goes here
-        </div>
-      </div>
+    </div>
+    <div className="bottom">
+      <div className="line">&nbsp;</div>
     </div>
   </div>
 );
