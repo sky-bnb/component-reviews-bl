9635d294cbabaa272446125709e8d7597e173d8d
bjlagman12 <bjlagman@gmail.com>
2019-05-21 18:27:38 -0700

    Implement read more functionality

--- src/componenets/ReviewListEntry.jsx
+++ src/componenets/ReviewListEntry.jsx
@@ -1,31 +1,60 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import '../ReviewListEntry.css';
 
-const ReviewListEntry = ({ review }) => (
-  <div className="eachReview">
-    <div className="top">
-      <div className="userAv">
-        <img src={review.avatar} alt="" />
-      </div>
-      <div className="userInfo">
-        <div className="userName">
-          {review.name}
+class ReviewListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
+  }
+
+  render() {
+    const { review } = this.props;
+    const { expanded } = this.state;
+    const toggledClass = expanded ? 'expanded' : 'collapsed';
+    const arr = review.review.split(' ');
+    let read;
+
+    if (arr.length > 40) {
+      read = (
+        <button
+          type="submit"
+          className="read"
+          onClick={() => this.setState({ expanded: true })}
+        >
+          {!expanded ? '...Read more' : ''}
+        </button>
+      );
+    }
+
+    return (
+      <div className="eachReview">
+        <div className="top">
+          <div className="userAv">
+            <img src={review.avatar} alt="" />
+          </div>
+          <div className="userInfo">
+            <div className="userName">
+              {review.name}
+            </div>
+            <div className="reviewDate">
+              {review.date_created}
+            </div>
+          </div>
         </div>
-        <div className="reviewDate">
-          {review.date_created}
+        <div className="mid">
+          <div className={toggledClass}>
+            {review.review}
+          </div>
+          {read}
+        </div>
+        <div className="bottom">
+          <div className="line">&nbsp;</div>
         </div>
       </div>
-    </div>
-    <div className="mid">
-      <div className="description">
-        {review.review}
-      </div>
-    </div>
-    <div className="bottom">
-      <div className="line">&nbsp;</div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default ReviewListEntry;
